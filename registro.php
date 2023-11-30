15a704dcb5e7fc282850462276ad5366776fa1e4
LuisRodrigoNR <luisrnr2910@gmail.com>
2023-11-29 22:20:27 -0600

    quitando basofia

--- registro.php
+++ registro.php
@@ -19,11 +19,6 @@
           <label for="last_name">Last Name</label>
         </div>
       </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-          <label for="disabled">Disabled</label>
-        </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
